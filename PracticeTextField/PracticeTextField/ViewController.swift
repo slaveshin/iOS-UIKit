25deb48eee17a62ebb15933bb70df5749d7d4c3a
shin seung jae <mok05247@naver.com>
2023-06-08 16:09:50 +0900

    PracticeTextField 추가

---
+++ PracticeTextField/PracticeTextField/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  PracticeTextField
+//
+//  Created by 신승재 on 2023/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 뷰컨트롤러가 텍스트 필드의 대리자를 해준다
+        textField.delegate = self
+        
+        setup()
+    }
+    
+    func setup() {
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .google
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        print(#function)
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    
+
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
+
---
+++ PracticeTextField/README.md
@@ -0,0 +1,9 @@
+# PracticeTextField(iOS)
+
+`storyboard` `text field` `delegate pattern`
+<br><br>
+text field의 델리게이트 패턴 연습용 어플리케이션
+
+### main screen
+
+<img src="" width="400"/>
