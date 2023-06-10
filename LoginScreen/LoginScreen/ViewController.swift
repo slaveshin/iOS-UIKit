898a69549b137148fb607f388298968f93b3bcc9
shin seung jae <mok05247@naver.com>
2023-06-10 17:24:11 +0900

    Login Screen 클로저 활용

--- LoginScreen/LoginScreen/ViewController.swift
+++ LoginScreen/LoginScreen/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let emailTextFieldView = UIView()
-    // let emailTextFieldView = UIView()
+    let emailTextFieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.darkGray
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +24,20 @@ class ViewController: UIViewController {
     }
     
     func makeUI() {
-        emailTextFieldView.backgroundColor = UIColor.darkGray
         view.addSubview(emailTextFieldView)
         
+        // 자동 오토레이아웃 끄기
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 수동으로 오토레이아웃 잡기
         emailTextFieldView.leadingAnchor
+            .constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
         emailTextFieldView.trailingAnchor
+            .constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
         emailTextFieldView.topAnchor
-        emailTextFieldView.heightAnchor
+            .constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
     }
 
 
