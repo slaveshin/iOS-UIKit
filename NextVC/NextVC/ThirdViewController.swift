5e3a819cfc16bdd361e46c5b4354cb1f3ec70eae
shin seung jae <mok05247@naver.com>
2023-06-30 10:18:33 +0900

    NextVC의 Second 뷰컨 수정

--- NextVC/NextVC/ThirdViewController.swift
+++ NextVC/NextVC/ThirdViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +19,9 @@ class ThirdViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
     }
-    */
+    
 
 }
