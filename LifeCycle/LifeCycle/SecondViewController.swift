e7bd7ba0fae8dfdd389348078c4f5099b08dd7ac
shin seung jae <mok05247@naver.com>
2023-07-06 22:29:18 +0900

    LifeCycle 완성

--- LifeCycle/LifeCycle/SecondViewController.swift
+++ LifeCycle/LifeCycle/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  LifeCycle
 //
 //  Created by 신승재 on 2023/07/06.
@@ -7,30 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("VC - 1 " + #function)
+        print("---> VC - 2 " + #function)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("VC - 1 " + #function)
+        print("---> VC - 2 " + #function)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("VC - 1 " + #function)
+        print("---> VC - 2 " + #function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        print("VC - 1 " + #function)
+        print("---> VC - 2 " + #function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-        print("VC - 1 " + #function)
+        print("---> VC - 2 " + #function)
     }
 
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        print("---> VC - 2 메모리에서 내려감")
+    }
 }
-
