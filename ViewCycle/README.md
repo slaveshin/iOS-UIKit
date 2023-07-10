ee137bfeb2edcf5de99525326458d01bd62697ed
shin seung jae <mok05247@naver.com>
2023-07-10 19:52:16 +0900

    View Cycle 프로젝트 추가

---
+++ ViewCycle/README.md
@@ -0,0 +1,15 @@
+# ViewCycle(iOS)
+
+`storyboard` `View Cycle`
+<br><br>
+뷰의 생명주기 공부용 앱<br>
+
+<br>
+
+### 뷰의 Life Cycle은
+
+<br>
+
+### Main Screen
+
+<img src="" width="450"/>
---
+++ ViewCycle/ViewCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewCycle
+//
+//  Created by 신승재 on 2023/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
