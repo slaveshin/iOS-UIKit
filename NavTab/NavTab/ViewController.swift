d325625f121957d970ad739f66e17fa779b929dc
shin seung jae <mok05247@naver.com>
2024-03-03 11:40:26 +0900

    Nav Tab 프로젝트 추가

---
+++ NavTab/NavTab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavTab
+//
+//  Created by 신승재 on 3/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NavTab/README.md
@@ -0,0 +1,7 @@
+# MyTab(iOS)
+
+`storyboard` `segment control`
+
+### main screen
+
+<img src="" width="300"/>
