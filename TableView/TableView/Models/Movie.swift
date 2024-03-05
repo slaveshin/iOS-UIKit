cf901fb2f46b8e8454090ca926e4a844921c0690
shin seung jae <mok05247@naver.com>
2024-03-05 23:34:36 +0900

    TableView 프로젝트 추가

---
+++ TableView/TableView/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by 신승재 on 3/4/24.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
